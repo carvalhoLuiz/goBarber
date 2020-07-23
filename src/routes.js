7044e0e79a75091509c6c6eac5cd9dacdb523ad4
Luiz Alberto R Carvalho <beto_carvvalho@hotmail.com>
2020-07-22 23:47:53 -0300

    fazendo conexao com o banco de dados

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/user';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'teste' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Luiz Alberto R. Carvalho',
+    email: 'beto_carvvalho@hotmail.com',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
