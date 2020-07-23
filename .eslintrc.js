89f1061ad1c908481794513db84aaa8fe92987e5
Luiz Alberto R Carvalho <beto_carvvalho@hotmail.com>
2020-07-22 21:55:16 -0300

    projeto iniciado (estudo goStack 9)

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": ["error", {
+    "endOfLine":"auto"
+    }],
+    'linebreak-style': 0,
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    // eslint-disable-next-line quote-props
+    'camelcase': 'off',
+    // eslint-disable-next-line quote-props
+    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
+  },
+};
