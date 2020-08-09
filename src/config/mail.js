2633a34e4e0156fadd39c39202fa4044afc0994a
Luiz Alberto R Carvalho <beto_carvvalho@hotmail.com>
2020-08-09 06:01:26 -0300

    configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'dc5dad2ba33d66',
+    pass: '1ebed39412c4d0',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
